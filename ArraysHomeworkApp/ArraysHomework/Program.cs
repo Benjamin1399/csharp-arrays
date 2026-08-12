5932c55f29f8c331332a4b9c7441390526a9acc1
Benjamin Naidoo <naidoobenjamin1313@gmail.com>
2026-08-12 19:54:32 +0200

    Uploaded Arrays project files

---
+++ ArraysHomeworkApp/ArraysHomework/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+string[] names = new string[] { "Ben", "Yash", "Smokey" };
+string indexText;
+
+do
+{
+    Console.Write("Enter array index: ");
+    indexText = Console.ReadLine();
+
+    if (indexText.ToLower() != "exit")
+    {
+        if (String.IsNullOrEmpty(indexText) == false)
+        {
+            if (int.TryParse(indexText, out int index))
+            {
+                if (index >= 0 && index < 3)
+                {
+                    Console.WriteLine($"Name chosen is {names[index]}");
+                }
+                else
+                {
+                    Console.WriteLine("Index out of bounds");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid index entered");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter an index");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Exiting");
+    }
+ 
+} while (indexText.ToLower() != "exit");
+
